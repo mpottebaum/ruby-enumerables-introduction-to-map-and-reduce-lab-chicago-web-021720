45dcc2631849e4e7157fb989bd45f0690d465e04
Mike Pottebaum <mpottebaum@gmail.com>
2020-01-27 01:12:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,4 +27,44 @@ def map_to_double(source_array)
 end
 
 def map_to_square(source_array)
-  new_array = 
+  new_array = []
+  index = 0
+  while index < source_array.length do
+    square = source_array[index] ** 2
+    new_array << square
+    index += 1
+  end
+  new_array
+end
+
+def reduce_to_total(source_array, starting_point)
+  total = 0
+  index = starting_point
+  while index < source_array.length do
+    total += source_array[index]
+    index += 1
+  end
+  total
+end
+
+def reduce_to_all_true(source_array)
+  index = 0
+  while index < source_array.length do
+    if source_array[index] == false
+      return false
+    end
+    index += 1
+  end
+  true
+end
+
+def reduce_to_any_true(source_array)
+  index = 0
+  while index < source_array.length do
+    if source_array[index] == true
+      return true
+    end
+    index += 1
+  end
+  false
+end
