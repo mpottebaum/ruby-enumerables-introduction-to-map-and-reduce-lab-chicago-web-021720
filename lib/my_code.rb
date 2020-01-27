9b365c4333cdf766f71fca2c67b48a3d33b73b9c
Mike Pottebaum <mpottebaum@gmail.com>
2020-01-27 01:06:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # My Code here....
+def map_to_negativize(source_array)
+  new_array = []
+  index = 0
+  while index < source_array.length do
+    negative = source_array[index] * -1
+    new_array << negative
+    index += 1
+  end
+  new_array
+end
+
+def map_to_no_change(source_array)
+  new_array = source_array
+  new_array
+end
+
+def map_to_double(source_array)
+  new_array = []
+  index = 0
+  while index < source_array.length do
+    double = source_array[index] * 2
+    new_array << double
+    index += 1
+  end
+  new_array
+end
+
+map_to_square(source_array)
